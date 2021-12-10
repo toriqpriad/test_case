88e56972ff1bce3e6eb29bbab1aca8eb2f48278d
Toriq Pria Dhigfora <toriqpriad@gmail.com>
2021-12-10 13:45:57 +0700

    latest commit

--- index.php
+++ index.php
@@ -51,8 +51,7 @@ $app = require_once __DIR__ . '/src/bootstrap/app.php';
 |
 */
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
+$kernel   = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
